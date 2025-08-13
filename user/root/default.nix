6b41c4d00f8d6520d36d1287773b4de65f666e5d
Liet Blue <lietblue@posteo.net>
2025-08-13 13:04:49 +0800

    fix: it is leaking

--- user/root/default.nix
+++ user/root/default.nix
@@ -8,7 +8,7 @@
 }:
 {
   users.users.root = {
-    hashedPassword = "$y$j9T$rs7PKfXvqv5i7IdHNM3Xu0$0p3ZpqnfI6O3jERB1hgppAdnTT5A.P8Yz6YNdvrZreD"; # s1CnO9g5QE5nYj
+    hashedPassword = "$y$j9T$oyHUzOw9l5wC24ZmNm44r1$NHqGpVwgI6w57qxyqWYXf3kcMZJ39AS7obSj78CGEe4";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHvAHjQDflHG4EGpJralY9rgmdn33qUbt17AU0AbDgF5 liet-key-20231122"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKRQW5QLkpJwkq9VGmhiUgd2glOarlyoCYNYz27W/yEn"
